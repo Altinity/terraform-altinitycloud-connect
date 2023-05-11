004a2de3ecd59640ca5242bc1e8341549b821313
Dan Mahoney <dan.mahoney@tatari.tv>
2023-05-11 09:08:12 -0400

    add support for namespace variables

--- ns.tf
+++ ns.tf
@@ -2,6 +2,7 @@ resource "kubernetes_namespace_v1" "altinitycloud_system" {
   metadata {
     name        = "altinity-cloud-system"
     annotations = var.namespace_annotations
+    labels      = var.namespace_labels
   }
 }
 
@@ -9,5 +10,6 @@ resource "kubernetes_namespace_v1" "altinitycloud_managed_clickhouse" {
   metadata {
     name        = "altinity-cloud-managed-clickhouse"
     annotations = var.namespace_annotations
+    labels      = var.namespace_labels
   }
 }
