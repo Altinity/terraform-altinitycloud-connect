89237593134ee57dc67e4e6ad0902d12d2aac438
Stanley Shyiko <stanley.shyiko@gmail.com>
2023-11-29 18:54:17 -0800

    Add use_external_secret variable to control whether cloud-connect secret is created by the module or supplied externally

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "system_namespace" {
+  value = kubernetes_namespace_v1.altinitycloud_system.metadata[0].name
+}
+
 output "clickhouse_namespace" {
   value = kubernetes_namespace_v1.altinitycloud_managed_clickhouse.metadata[0].name
   depends_on = [
